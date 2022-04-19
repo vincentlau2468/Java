2766d60584e14b2ca1bd0614bd7a1b6ad38144f8
vincentlau2468 <vincentyam2468@gmail.com>
2022-04-19 15:41:13 +0800

    1

---
+++ Q1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int first = scn.nextInt();
+        int second = scn.nextInt();
+        System.out.println(first + second);
+        scn.close();
+    }
+}
